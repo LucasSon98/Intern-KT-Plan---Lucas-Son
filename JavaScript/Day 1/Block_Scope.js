a0a8f0e2d6bcbf0c8e60f08eb6918058a634920a
LucasSon98 <86804196+LucasSon98@users.noreply.github.com>
2023-02-24 09:08:49 -0300

    Add files via upload

--- JavaScript/Day 1/Block_Scope.js
+++ JavaScript/Day 1/Block_Scope.js
@@ -16,3 +16,29 @@ if(true){
 console.log('outside code block: ', age, name)
 
 // the concept of scope works the same for const defined variables
+// variables created with var statement are always global scope variables
+
+// Example:
+
+if (Math.random() > 0.5) {
+  let x = 1;
+  console.log(y); // This will return an error since y only exist in the else block
+} else {
+  let y = 2;
+  console.log(x); // This will return an error since x only exist in the else block
+} 
+console.log(x); // This will return an error since x and y only exist in the if and else block respectively 
+
+// However if we change the let statement with the var statement since all variables become global variables
+// none of the console.log() will return an error.
+// Note that variables might be undefined until the conditions in the if or else statements are met
+// , but no error will be thrown.
+
+if (Math.random() > 0.5) {
+  var x = 1;
+  console.log(y);
+} else {
+  var y = 2;
+  console.log(x); 
+} 
+console.log(x);
