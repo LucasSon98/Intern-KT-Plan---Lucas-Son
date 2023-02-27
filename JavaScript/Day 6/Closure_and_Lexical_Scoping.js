76aa2f1f687341a5430922818eacf6f5315a5494
LucasSon98 <86804196+LucasSon98@users.noreply.github.com>
2023-02-27 09:56:20 -0300

    Add files via upload

--- JavaScript/Day 6/Closure_and_Lexical_Scoping.js
+++ JavaScript/Day 6/Closure_and_Lexical_Scoping.js
@@ -139,7 +139,27 @@ console.log(counter.value()); // 1.
 // The public elements are the 'increment', 'decrement' and 'value' functions,
 // which have access to the private elements thanks to the lexical scoping, forming closures that share the same lexical environment.
 
-// Similarly to Example 2 we can create to completely independent counters with their own lexical environment.
+// Similarly to Example 2 we can create two completely independent counters with their own lexical environment.
+
+const makeCounter = function () {
+  let privateCounter = 0;
+  function changeBy(val) {
+    privateCounter += val;
+  }
+  return {
+    increment() {
+      changeBy(1);
+    },
+
+    decrement() {
+      changeBy(-1);
+    },
+
+    value() {
+      return privateCounter;
+    },
+  };
+};
 
 const counter1 = makeCounter();
 const counter2 = makeCounter();
